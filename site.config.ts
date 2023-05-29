f71010abe49ba790a1d18bcb2f158fa029555013
Jawsh Mizzle <jawsh@gmx.com>
2023-05-29 05:00:06 +0000

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,19 +1,20 @@
 export default {
   // your domain name
-  host: "zaps.lol",
+  host: "pinecones.lol",
 
   // set to true to disable registration site-wide
   registrationDisabled: false,
 
   // site slogan
-  slogan: "Free nostr addresses.",
+  slogan: "Free NIP-05 verification.",
 
   // your monthly costs
-  costsMonthly: "$4 USD",
+  costsMonthly: "$1.25 USD",
 
   // your donation LNURL
   donationLNUrl:
-    "LNURL1DP68GURN8GHJ7MR9VAJKUEPWD3HXY6T5WVHXXMMD9AKXUATJD3CZ7JMPVDV9YCC4FVREX",
+    "LNURL1DP68GURN8GHJ7EM9W3SKCCNE9E3K7MF0D3H82UNVWQHHQ6TWV43K7MN9WV25WM70",
+
 
   // name of your MongoDB database
   dbName: "users",
